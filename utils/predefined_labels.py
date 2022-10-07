11a93a35bc3ebaadfd41f10886b17b1198baba2d
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-10-07 20:16:25 +0530

    checkpoint

--- utils/predefined_labels.py
+++ utils/predefined_labels.py
@@ -7,10 +7,37 @@ BBOX_LABEL_CONFIG = \
 <View>
   <Image name="image" value="$image"/>
   <RectangleLabels name="label" toName="image">
-    <Label value="Airplane" background="green"/>
-    <Label value="Car" background="blue"/>
+    <Label value="Angry" background="#FFA39E"/>
+    <Label value="Disgust" background="#D4380D"/>
+    <Label value="Fear" background="#FFC069"/>
+    <Label value="Happy" background="#AD8B00"/>
+    <Label value="Sad" background="#D3F261"/>
+    <Label value="Surprise" background="#389E0D"/>
+    <Label value="Neutral" background="#5CDBD3"/>
   </RectangleLabels>
 </View>
+"""
+
+VIDEO_TIMELINE_SEGMENTATION_LABEL_CONFIG = \
+"""
+<View>
+  <Header value="Video timeline segmentation via AudioPlus sync trick"/>
+  <Video name="video" value="$image" sync="audio"/>
+  <Labels name="tricks" toName="audio" choice="multiple">
+    <Label value="Angry" background="#FFA39E"/>
+    <Label value="Disgust" background="#D4380D"/>
+    <Label value="Fear" background="#FFC069"/>
+    <Label value="Happy" background="#AD8B00"/>
+    <Label value="Sad" background="#D3F261"/>
+    <Label value="Surprise" background="#389E0D"/>
+    <Label value="Neutral" background="#5CDBD3"/>
+  </Labels>
+  <AudioPlus name="audio" value="$image" sync="video" speed="false"/>
+</View><!--
+  Audio tag uses the same $video file to be in sync, video is muted
+--><!--{
+ "video_url": "/static/samples/opossum_snow.mp4"
+}-->
 """
 
 get_bbox_label_config = lambda: BBOX_LABEL_CONFIG
